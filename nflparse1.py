79ed2b5e4e5e030dae6b8d840d237fdbbadd6bb5
Kartapuza <a>
2017-10-24 22:37:15 +0300

    n_241017

--- nflparse1.py
+++ nflparse1.py
@@ -24,53 +24,39 @@ def GetUrl():
  with open('test.html', 'w') as output_file:
   output_file.write(r.text)
 
-def startGrab():
- nflsite_1 = open('test.html', 'r')
- my_string = nflsite_1.read()
-
- print(len(my_string))
- save_file(directory_data + 'nf1.txt', my_string)
-
- soup = BeautifulSoup(my_string, 'html.parser')
-
- letters = soup.find('div', {'class': 'new-score-box'})
- print(len(letters))
-
- ListP1=list(soup.find_all('p'))
- #print(ListP1)
- print(len(ListP1))
-
- seven_day = soup.find('div', {'class': 'new-score-box'})
- forecast_items = seven_day.find_all(class_="new-score-box")
-
- short_descs = [sd.get_text() for sd in seven_day.select(".new-score-box .team-name")]
- temps = [t.get_text() for t in seven_day.select(".new-score-box .team-record")]
- #descs = [d["title"] for d in seven_day.select(".new-score-box img")]
-
- new_score = soup.find('p', {'class': 'team-name'})
-
- team11 = soup.find('div', {'class': 'new-score-box'})
- team12 = soup.find('div', {'class': 'new-score-box'})
-
- if team11 == team12:
-    print('ok')
-
- team11_away = team11.find('div', {'class': 'away-team'}).find('a').get('href')
- team12_home = team12.find('p', {'class': 'team-name'}).find('a').text
- team12_home_stat = team12.find('p', {'class': 'team-record'}).text
-
 def find_week_games():
     nflsite_1 = open('test.html', 'r')
     my_string = nflsite_1.read()
+
+    save_file(directory_data + 'nf1.txt', my_string)
+
     soup = BeautifulSoup(my_string, 'html.parser')
-    seven_day = soup.find(id="score-boxes")
-    forecast_items = seven_day.find_all(class_="new-score-box")
-    tonight = forecast_items[0]
-    print(tonight.prettify())
+    week_scores = soup.find(id="score-boxes")
+    
+    game_team = week_scores.find_all(class_="new-score-box")
+    tonight = game_team[0]
+    #print(tonight.prettify())
+
+    away_team = tonight.find(class_="away-team")
+    period = away_team.find(class_="team-name").get_text()
+    short_desc = away_team.find(class_="total-score").get_text()
+    temp = away_team.find(class_="team-record").get_text()
+
+    print(period)
+    print(short_desc)
+    print(temp)
+
+    home_team = tonight.find(class_="home-team")
+    period = home_team.find(class_="team-name").get_text()
+    short_desc = home_team.find(class_="total-score").get_text()
+    temp = home_team.find(class_="team-record").get_text()
+
+    print(period)
+    print(short_desc)
+    print(temp)
 
 
 if __name__ == '__main__':
     ensure_dir(directory_data)
-    GetUrl()
-    startGrab()
+    #GetUrl()
     find_week_games()
