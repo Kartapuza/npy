c7037abe473b67fa3a6d19e8c79fb7ea4347404e
Dev2 <Dev2>
2017-11-08 22:32:25 +0300

    081117 https except esp_n sett

--- nflparse1.py
+++ nflparse1.py
@@ -1,6 +1,7 @@
 import os
-import requests
-from bs4 import BeautifulSoup
+import requests  #__version__ = '2.18.4'
+from bs4 import BeautifulSoup #__version__ = "4.0.0b"
+import re
 
 #Создание структуры каталогов
 current_dir = os.getcwd()
@@ -16,61 +17,158 @@ def save_file(name_file, lines):
  save_changes.close()
 
 def GetUrl():
- url = 'http://www.nfl.com/scores'
+ #url_nfl = 'http://www.nfl.com/scores'
+ url_nfl = 'http://www.nfl.com/scores/2017/REG10'
+
+ url_powerrankings = 'https://www.cbssports.com/nfl/powerrankings/'
+
+ url_espn = 'http://www.espn.com/nfl/picks/_/week/10'
+ url_cbs = 'https://www.cbssports.com/nfl/features/writers/expert/picks/straight-up/10'
+
+
+ url_bwin = 'https://www.bwin.com/'
  headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
  }
- r = requests.get(url, headers=headers)
- with open('test.html', 'w') as output_file:
+ r = requests.get(url_nfl, headers=headers)
+ with open('scores.html', 'w') as output_file:
+  output_file.write(r.text)
+ r = requests.get(url_powerrankings , headers=headers)
+ with open('power.html', 'w') as output_file:
+  output_file.write(r.text)
+ r = requests.get(url_espn, headers=headers)
+ with open('espn.html', 'w') as output_file:
   output_file.write(r.text)
 
-def startGrab():
- nflsite_1 = open('test.html', 'r')
- my_string = nflsite_1.read()
-
- print(len(my_string))
- save_file(directory_data + 'nf1.txt', my_string)
-
- soup = BeautifulSoup(my_string, 'html.parser')
-
- letters = soup.find('div', {'class': 'new-score-box'})
- print(len(letters))
-
- ListP1=list(soup.find_all('p'))
- #print(ListP1)
- print(len(ListP1))
-
- seven_day = soup.find('div', {'class': 'new-score-box'})
- forecast_items = seven_day.find_all(class_="new-score-box")
-
- short_descs = [sd.get_text() for sd in seven_day.select(".new-score-box .team-name")]
- temps = [t.get_text() for t in seven_day.select(".new-score-box .team-record")]
- #descs = [d["title"] for d in seven_day.select(".new-score-box img")]
-
- new_score = soup.find('p', {'class': 'team-name'})
-
- team11 = soup.find('div', {'class': 'new-score-box'})
- team12 = soup.find('div', {'class': 'new-score-box'})
-
- if team11 == team12:
-    print('ok')
-
- team11_away = team11.find('div', {'class': 'away-team'}).find('a').get('href')
- team12_home = team12.find('p', {'class': 'team-name'}).find('a').text
- team12_home_stat = team12.find('p', {'class': 'team-record'}).text
+ try:
+  r = requests.get(url_cbs, headers=headers, verify=False, timeout=(10, 1))
+ except requests.exceptions.ReadTimeout:
+    print('Oops. Read timeout occured')
+ except requests.exceptions.ConnectTimeout:
+    print('Oops. Connection timeout occured!')
+ with open('cbs.html', 'w') as output_file:
+  output_file.write(r.text)
 
 def find_week_games():
-    nflsite_1 = open('test.html', 'r')
+    nflsite_1 = open('scores.html', 'r')
     my_string = nflsite_1.read()
+
+    save_file(directory_data + 'nf1.txt', my_string)
+
     soup = BeautifulSoup(my_string, 'html.parser')
-    seven_day = soup.find(id="score-boxes")
-    forecast_items = seven_day.find_all(class_="new-score-box")
-    tonight = forecast_items[0]
-    print(tonight.prettify())
+    week_scores = soup.find(id="score-boxes")
+    game_team = week_scores.find_all('div', attrs={"class":"new-score-box"})
+
+    for i in range(0, len(game_team)):
+        tonight = game_team[i]
+
+        away_team = tonight.find('div', attrs={"class":"away-team"})
+        away_name = away_team.find('p', attrs={"class":"team-name"}).get_text()
+        away_score = away_team.find('p', attrs={"class":"total-score"}).get_text()
+        away_record = away_team.find('p', attrs={"class":"team-record"}).get_text()
+
+        if away_team.find('p', attrs={"class": "quarters-score"}) is None:
+            quarters_score = 0
+        else:
+            quarters_score = 1
+            score_first_qt = away_team.find('span', attrs={"class": "first-qt"}).get_text()
+            score_second_qt = away_team.find('span', attrs={"class": "second-qt"}).get_text()
+            score_third_qt = away_team.find('span', attrs={"class": "third-qt"}).get_text()
+            score_fourth_qt = away_team.find('span', attrs={"class": "fourth-qt"}).get_text()
+            score_ot_qt = away_team.find('span', attrs={"class": "ot-qt"}).get_text()
+
+        print(away_name)
+        print(away_record.strip())
+        print(away_score.strip())
+        if int(quarters_score) > 0:
+          print(score_first_qt + ' ' + score_second_qt + ' ' + score_third_qt + ' ' + score_fourth_qt + ' ' + score_ot_qt )
+        else:
+          print('_ ' + '_ ' + '_ ' + '_ ')
+
+        home_team = tonight.find('div', attrs={"class":"home-team"})
+        home_name =  home_team.find('p', attrs={"class":"team-name"}).get_text()
+        home_score =  home_team.find('p', attrs={"class":"total-score"}).get_text()
+        home_record = home_team.find('p', attrs={"class":"team-record"}).get_text()
+
+        if home_team.find('p', attrs={"class": "quarters-score"}) is None:
+            quarters_score = 0
+        else:
+            quarters_score = 1
+            score_first_qt = home_team.find('span', attrs={"class": "first-qt"}).get_text()
+            score_second_qt = home_team.find('span', attrs={"class": "second-qt"}).get_text()
+            score_third_qt = home_team.find('span', attrs={"class": "third-qt"}).get_text()
+            score_fourth_qt = home_team.find('span', attrs={"class": "fourth-qt"}).get_text()
+            score_ot_qt = home_team.find('span', attrs={"class": "ot-qt"}).get_text()
+
+        print(home_name)
+        print(home_record.strip())
+        print(home_score.strip())
+        if int(quarters_score) > 0:
+          print(score_first_qt + ' ' + score_second_qt + ' ' + score_third_qt + ' ' + score_fourth_qt + ' ' + score_ot_qt )
+        else:
+          print('_ ' + '_ ' + '_ ' + '_ ')
+
+def power_rankings():
+    nflsite_1 = open('power.html', 'r')
+    my_string = nflsite_1.read()
+
+    soup = BeautifulSoup(my_string, 'html.parser')
+    week_power = soup.find("div", attrs={"class":"table-wrapper"})
+    game_team = week_power.find_all('tr', attrs={"class":"player-rankings-stats"})
+
+    for i in range(0, len(game_team)):
+        tonight = game_team[i]
+
+        team_rank = tonight.find('span', attrs={"class":"rank"}).get_text()
+
+        if tonight.find('td', attrs={"class": "cell-left change-up"}) is None:
+             team_rank_up = 0
+        else:
+             team_rank_up = tonight.find('td', attrs={"class": "cell-left change-up"}).get_text()
+
+        if tonight.find('td', attrs={"class": "cell-left change-down"})  is None:
+             team_rank_down = 0
+        else:
+             team_rank_down = tonight.find('td', attrs={"class": "cell-left change-down"}).get_text()
+
+        team_stats = tonight.find_all('td', attrs={"class": "cell-left"})
+        team_stat = team_stats[1].get_text()
+
+        team_name = tonight.find('td', attrs={"class":"cell-left team"}).get_text()
+        team_comment = tonight.find('td', attrs={"class":"cell-left dek"}).get_text()
+
+        print(team_rank.strip())
+
+        if int(team_rank_up) > 0:
+            print('+' + team_rank_up.strip())
+        elif int(team_rank_down) > 0:
+            print('-' + team_rank_down.strip())
+        else:
+            print('`')
+        print(team_name.strip())
+        print(team_stat.strip())
+        print(team_comment.strip())
+
+def espn_read():
+    espn_1 = open('espn.html', 'r')
+    my_string = espn_1.read()
+
+    soup = BeautifulSoup(my_string, 'html.parser')
+    week_pick_espn = soup.find("table", attrs={"class": "tablehead"})
+    game_pick = week_pick_espn.find_all('tr', attrs={"class": re.compile(r'[a-z]||row team||r''[a-z]')})
+    for i in range(0, len(game_pick)):
+        tonight = game_pick[i]
+        game_day_pick = tonight.find_all('div', attrs={"class": re.compile(r'[a-z]||teampick logo||r''[a-z]')})
+        for j in range(0, len(game_day_pick)):
+            topick = game_day_pick[j]
+            print(game_day_pick[j].get_text())
+        print('---')
 
 
 if __name__ == '__main__':
     ensure_dir(directory_data)
     GetUrl()
-    startGrab()
+    power_rankings()
     find_week_games()
+    espn_read()
+
