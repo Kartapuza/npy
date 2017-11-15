3efc4e3302b31e684e5d78f228aaedb965915f39
Your Name <you@example.com>
2017-11-16 00:23:14 +0300

    Merge branch 'a_home' of https://github.com/Kartapuza/npy into a_home 16.11.17
    # Conflicts:
    #	no

--- statversusteam.py
+++ statversusteam.py
@@ -10,25 +10,36 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def Load_Settings():
-    global url_stat
-    url_stat = 'none'
+    global url_scores_stat, url_team_stat
+    url_scores_stat = url_team_stat ='none'
     settings_1 = open('settingsstat.txt', 'r')
     my_string = settings_1.readlines()
     if my_string[0].strip() == '1':
-        url_stat = my_string[1].strip()
-    else: url_stat = 'none'
+        url_scores_stat = my_string[1].strip()
+    else: url_scores_stat = 'none'
+
+    if my_string[2].strip() == '1':
+        url_team_stat = my_string[3].strip()
+    else:
+        url_team_stat = 'none'
+
 
 def GetUrl():
  headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
  }
- if len(url_stat) > 5:
-    r = requests.get(url_stat, headers=headers)
+ if len(url_scores_stat) > 5:
+    r = requests.get(url_scores_stat, headers=headers)
     with open('statsall.html', 'w', encoding='utf-8') as output_file:
         output_file.write(r.text)
 
+ if len(url_team_stat) > 5:
+     r = requests.get(url_team_stat, headers=headers)
+     with open('statsteam.html', 'w', encoding='utf-8') as output_file:
+         output_file.write(r.text)
 
-def Read_Stat():
+
+def Read_All_Stats():
     stat_1 = open('statsall.html', 'r')
     my_string = stat_1.read()
 
@@ -52,8 +63,30 @@ def Read_Stat():
         team_link1A = team_link2B = ''
         print('s--')
 
+def Read_Stats_Team():
+    stat_2 = open('statsteam.html', 'r')
+    my_string2 = stat_2.read()
+    soup = BeautifulSoup(my_string2, 'html.parser')
+    #stat_team = soup.find("div", attrs={"id": "chalk_stats"})
+    team_city = soup.find('div', attrs={"class": "team-city"}).get_text()
+    team_nickname = soup.find('div', attrs={"class": "team-nickname"}).get_text()
+
+    print(team_city)
+    print(team_nickname)
+
+    #allgame_stats = stat_team.find_all('td', attrs={"class": re.compile('(team|score|time)')})
+    #allgame_team_name =  stat_team.find_all('td', attrs={"class": "team"})
+    #allgame_team_score = stat_team.find_all('td', attrs={"class": "score"})
+    #allgame_team_time = stat_team.find_all('td', attrs={"class": "time"})
+    #---
+
+
 if __name__ == '__main__':
-    sys.stdout = open('resultstat.out', 'w')
     Load_Settings()
     GetUrl()
-    Read_Stat()
+    if len(url_scores_stat) > 5:
+        sys.stdout = open('resultstat.out', 'w')
+        Read_All_Stats()
+    if len(url_team_stat) > 5:
+        sys.stdout = open('resultstatteam.out', 'w')
+        Read_Stats_Team()
